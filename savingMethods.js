a0263f0823cece0710ecf5b6636cf6c808d18c4d
cbarbosa2016 <christabelbarbosa@Christabels-MacBook-Pro.local>
2018-07-07 12:31:13 -0400

    Added savingMethods.js and yourGoals.js

---
+++ savingMethods.js
@@ -0,0 +1,16 @@
+import { Button, Title, SubTitle} from 'reactbulma';
+import React, {Component} from 'react';
+
+class savingMethods extends Component {
+    render() {
+        return (
+                <div>
+                </div>
+                )
+    }
+}
+
+
+export default savingMethods;
+
+
---
+++ yourGoals.js
@@ -0,0 +1,16 @@
+import { Button, Title, SubTitle} from 'reactbulma';
+import React, {Component} from 'react';
+
+class yourGoals extends Component {
+    render() {
+        return (
+                <div>
+                </div>
+                )
+    }
+}
+
+
+export default yourGoals;
+
+
